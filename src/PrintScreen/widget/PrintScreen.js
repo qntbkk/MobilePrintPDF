23a0e47de8a10c6ccaf038df3a3ab5287624574c
Eric Tieniber <tieniber@gmail.com>
2016-09-01 18:07:45 -0400

    fully working with page splits

--- src/PrintScreen/widget/PrintScreen.js
+++ src/PrintScreen/widget/PrintScreen.js
@@ -22,125 +22,152 @@ define([
     "mxui/widget/_WidgetBase",
     "dijit/_TemplatedMixin",
 
-	"PrintScreen/widget/lib/html2canvas",
-	"PrintScreen/widget/lib/jspdf",
+    "PrintScreen/widget/lib/html2canvas",
+    "PrintScreen/widget/lib/jspdf",
     "dojo/dom-class",
     "dojo/_base/lang",
-	"dojo/query",
-	"dojo/on",
-	"dojo/date/locale",
+    "dojo/query",
+    "dojo/on",
+    "dojo/date/locale",
     "dojo/text!PrintScreen/widget/template/PrintScreen.html"
-], function (declare, _WidgetBase, _TemplatedMixin, html2canvas, JsPDF, dojoClass, dojoLang, dojoQuery, on, locale, widgetTemplate) {
-	"use strict";
+], function(declare, _WidgetBase, _TemplatedMixin, html2canvas, JsPDF, dojoClass, dojoLang, dojoQuery, on, locale, widgetTemplate) {
+    "use strict";
 
-	// Declare widget's prototype.
-	return declare("PrintScreen.widget.PrintScreen", [_WidgetBase, _TemplatedMixin], {
-		// _TemplatedMixin will create our dom node using this HTML template.
-		templateString: widgetTemplate,
+    // Declare widget's prototype.
+    return declare("PrintScreen.widget.PrintScreen", [_WidgetBase, _TemplatedMixin], {
+        // _TemplatedMixin will create our dom node using this HTML template.
+        templateString: widgetTemplate,
 
-		// DOM elements
-		theButton: null,
+        // DOM elements
+        theButton: null,
 
-		// Parameters configured in the Modeler.
-		targetClass: "",
-		buttonText: "",
+        // Parameters configured in the Modeler.
+        targetClass: "",
+        buttonText: "",
+        orientation: null,
 
-		myHtml2canvas: null,
+        // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
+        myHtml2canvas: null,
 
-		// Internal variables. Non-primitives created in the prototype are shared between all widget instances.
-		// -------------------------------------------------------------------
-		// Constants for print table functionality
-		// -------------------------------------------------------------------
-		C_PRINTTABLE_X_POS_START: 80,
-		C_PRINTTABLE_Y_POS_START: 100,
-		C_PRINTTABLE_PIXELS_2_PDF_FACTOR: 0.9,
+        // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
+        postCreate: function() {
+            logger.debug(this.id + ".postCreate");
 
-		// dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
-		postCreate: function () {
-			logger.debug(this.id + ".postCreate");
+            this.theButton.innerHTML = this.buttonText;
+            on(this.theButton, "click", dojoLang.hitch(this, this.createPrintscreen));
+        },
 
-			this.theButton.innerHTML = this.buttonText;
-			on(this.theButton, "click", dojoLang.hitch(this, this.createPrintscreen));
-		},
+        createPrintscreen: function() {
+            var widgetNode = dojoQuery("." + this.targetClass)[0],
+                self = this;
 
-		createPrintscreen: function () {
-			var widgetNode = dojoQuery("." + this.targetClass)[0],
-				self = this;
+            //dojoClass.toggle("fixedWidth");
+            var pid = mx.ui.showProgress(null, true);
 
-			//dojoClass.toggle("fixedWidth");
+            html2canvas(widgetNode, {
+                onrendered: function(canvas) {
+                    // document.body.appendChild(canvas);
 
-			html2canvas(widgetNode, {
-				onrendered: function (canvas) {
-					// document.body.appendChild(canvas);
+                    var strFileName2Save = self.Filename2Save(self.targetClass),
+                        doc;
+                    //imgDatap1 = canvas.toDataURL("image/png").slice("data:image/png;base64,".length),
+                    //imgData = window.atob(imgDatap1),
+                    if (self.orientation === "portrait") {
+                        doc = new JsPDF("p", "pt", "letter", true);
+                    } else {
+                        doc = new JsPDF("l", "pt", "letter", true);
+                    }
 
-					var strFileName2Save = self.Filename2Save(self.targetClass),
-						//imgDatap1 = canvas.toDataURL("image/png").slice("data:image/png;base64,".length),
-						//imgData = window.atob(imgDatap1),
-						doc = new JsPDF("l", "pt", "letter");
 
-					var imgData = canvas.toDataURL('image/png');
+                    var imgData = canvas.toDataURL('image/png');
+                    var pageWidth, pageHeight;
 
-					/*
-					Here are the numbers (paper width and height) that I found to work.
-					It still creates a little overlap part between the pages, but good enough for me.
-					if you can find an official number from jsPDF, use them.
-					*/
-					var imgWidth = 792;
-					var pageHeight = 612;
-					var imgHeight = canvas.height * imgWidth / canvas.width;
-					var heightLeft = imgHeight;
+                    if (self.orientation === "portrait") {
+                        pageWidth = 612;
+                        pageHeight = 792;
+                    } else {
+                        pageWidth = 792;
+                        pageHeight = 612;
+                    }
 
-					//var doc = new jsPDF('p', 'mm');
-					var position = 0;
+                    //each page should be this number of pixels tall
+                    var imgPageHeight = canvas.width * pageHeight / pageWidth;
+                    var heightLeft = canvas.height;
 
-					doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-					heightLeft -= pageHeight;
+                    //var position = 0;
 
-					while (heightLeft >= 0) {
-						position = heightLeft - imgHeight;
-						doc.addPage();
-						doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-						heightLeft -= pageHeight;
-					}
-					//doc.save( 'file.pdf');﻿
+                    var imageObj = new Image();
+                    imageObj.src = imgData;
 
-					//doc.addImage(imgData, "png", 0, 0, 792, 0);
+                    var pageCanvas,
+                        context,
+                        pageImgData,
+                        pageCount = 0;
 
-					if (self.msieversion() > 8 && self.msieversion() < 11) {
-						doc.save();
-					} else {
-						doc.output("save", strFileName2Save);
-					}
-				},
-				height: widgetNode.scrollHeight
-			});
-		},
+                    while (heightLeft >= 0) {
 
-		msieversion: function () {
-			var ua = window.navigator.userAgent,
-				msie = ua.indexOf("MSIE ");
+                        if(pageCount !== 0) {
+                            doc.addPage();
+                        }
 
-			if (msie > 0) { // If Internet Explorer, return version number
-				return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)), 10);
-			} else { // If another browser, return 0
-				return 0;
-			}
-		},
+                        pageCanvas = document.createElement('canvas');
+                        pageCanvas.width = canvas.width;
+                        pageCanvas.height = imgPageHeight;
+                        context = pageCanvas.getContext('2d');
 
-		Filename2Save: function (Class2Print) {
-			var milsecDateNow = Date.now(),
-				dateDateNow = new Date(milsecDateNow),
-				options = {
-					selector: "date",
-					datePattern: "yyyy-MM-dd"
-				},
-				strDate = locale.format(dateDateNow, options),
-			// strTime = formatDate(dateDateNow, "hh:mm" );
+                        context.drawImage(imageObj, 0, canvas.height-heightLeft, canvas.width,  imgPageHeight, 0, 0, pageCanvas.width, pageCanvas.height);
 
-				strFileName2Save = this.fileNamePrefix + " " + strDate + ".pdf";
-			return strFileName2Save;
-		}
-	});
+                        pageImgData = pageCanvas.toDataURL('image/png');
+
+                        doc.addImage(pageImgData, 'PNG', 0, 0, pageWidth, pageHeight, pageCount, 'FAST');
+
+                        //doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+                        //position = heightLeft - imgPageHeight;
+                        heightLeft -= imgPageHeight;
+                        pageCount += 1;
+
+                    }
+                    //doc.save( 'file.pdf');﻿
+
+                    //doc.addImage(imgData, "png", 0, 0, 792, 0);
+
+                    if (self.msieversion() > 8 && self.msieversion() < 11) {
+                        doc.save();
+                    } else {
+                        doc.output("save", strFileName2Save);
+                    }
+
+                    mx.ui.hideProgress(pid);
+                },
+                height: widgetNode.scrollHeight
+            });
+        },
+
+        msieversion: function() {
+            var ua = window.navigator.userAgent,
+                msie = ua.indexOf("MSIE ");
+
+            if (msie > 0) { // If Internet Explorer, return version number
+                return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)), 10);
+            } else { // If another browser, return 0
+                return 0;
+            }
+        },
+
+        Filename2Save: function(Class2Print) {
+            var milsecDateNow = Date.now(),
+                dateDateNow = new Date(milsecDateNow),
+                options = {
+                    selector: "date",
+                    datePattern: "yyyy-MM-dd"
+                },
+                strDate = locale.format(dateDateNow, options),
+                // strTime = formatDate(dateDateNow, "hh:mm" );
+
+                strFileName2Save = this.fileNamePrefix + " " + strDate + ".pdf";
+            return strFileName2Save;
+        }
+    });
 });
 
 require(["PrintScreen/widget/PrintScreen"]);
